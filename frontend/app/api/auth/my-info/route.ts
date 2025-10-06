a6e9b5cc429672c60b9d22eb73d00b7427e6e3b1
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-10-06 22:54:29 +0900

    feat: 고차함수를 사용하여 retry fetch 구현

--- frontend/app/api/auth/my-info/route.ts
+++ frontend/app/api/auth/my-info/route.ts
@@ -1,6 +1,6 @@
 import {NextRequest, NextResponse} from "next/server";
 import {cookies} from "next/headers";
-import refreshAuthTokens from "@/utils/refreshToken";
+import refreshAuthTokens, {withRetry} from "@/utils/refreshToken";
 
 async function fetchMyInfo(accessToken: String){
     const target = `${process.env.NEXT_PUBLIC_KEYCLOAK_URL}/realms/${process.env.NEXT_PUBLIC_KEYCLOAK_REALM}/protocol/openid-connect/userinfo`;
@@ -21,47 +21,11 @@ export async function GET(req: NextRequest){
     }
 
     try {
-        const fetchResp = await fetchMyInfo(accessToken);
-
-        if(!fetchResp.ok){
-            if(fetchResp.status === 401){
-                console.log("[/api/proxy/login] 토큰 만료 재발급 필요");
-
-                const refreshToken = cookieStore.get('refresh_token')?.value;
-                if(!refreshToken){
-                    return NextResponse.json({error: 'Refresh token not found'}, {status: 401});
-                }
-
-                const tokens = await refreshAuthTokens(refreshToken);
-
-                console.log("토큰 재발급 완료");
-                const fetchResp = await fetchMyInfo(tokens.access_token);
-                console.log("토큰 재발급 이후 fetch", fetchResp.status);
-                if(fetchResp.ok){
-                    const resp = NextResponse.json(await fetchResp.json());
-
-                    // access 토큰 저장
-                    resp.cookies.set('access_token', tokens.access_token, {
-                        httpOnly: true,
-                        path: '/'
-                    });
-
-                    // refresh 토큰 저장
-                    resp.cookies.set('refresh_token', tokens.refresh_token, {
-                        httpOnly: true,
-                        path: '/'
-                    });
-
-                    return resp;
-                }
-            }
-            return NextResponse.json({
-                error: 'Failed to fetch'
-            }, {status: 400});
-        }
-
-        const userinfo = await fetchResp.json();
-        return NextResponse.json(userinfo);
+        const fetchResponse = await withRetry(accessToken, async (accessToken: string): Promise<Response>  => {
+            return fetchMyInfo(accessToken);
+        });
+        const responseJson = await fetchResponse.json();
+        return NextResponse.json(responseJson);
     }catch (error){
         console.log(error);
         return NextResponse.json({error: 'SERVER ERROR'}, {status: 500});
--- frontend/utils/refreshToken.ts
+++ frontend/utils/refreshToken.ts
@@ -1,4 +1,5 @@
 import {NextResponse} from "next/server";
+import {cookies} from "next/headers";
 
 export default async function refreshAuthTokens (refreshToken: string): tokens{
     const target = `${process.env.NEXT_PUBLIC_KEYCLOAK_URL}/realms/${process.env.NEXT_PUBLIC_KEYCLOAK_REALM}/protocol/openid-connect/token`;
@@ -22,4 +23,40 @@ export default async function refreshAuthTokens (refreshToken: string): tokens{
         access_token: tokens.access_token,
         refresh_token: tokens.refresh_token,
     };
+}
+
+export async function withRetry(accessToken: string, f : (accessToken: string) => Promise<Response>){
+    try {
+        console.log("[withRetry] withRetry");
+        const fetchResponse = await f(accessToken);
+        if(fetchResponse.status === 401){
+            // 토큰 재발급
+            console.log("[withRetry] 토큰 재발급");
+            const cookieStore = cookies();
+            const refreshToken = cookieStore.get("refresh_token")?.value;
+
+            if(!refreshToken){
+                throw new Error("refresh token not found");
+            }
+
+            console.log("[withRetry] 토큰 재발급 성공");
+
+            const tokens: tokens = await refreshAuthTokens(refreshToken);
+            const retryResponse = await f(tokens.access_token);
+            if(retryResponse.ok){
+                console.log("[withRetry] 재시도 성공");
+
+                cookies().set('access_token', tokens.access_token, {httpOnly: true});
+                cookies().set('refresh_token', tokens.refresh_token, {httpOnly: true});
+                return retryResponse;
+            }else {
+                return fetchResponse;
+            }
+        }else {
+            return fetchResponse;
+        }
+    }catch (error){
+        console.log("[withRetry]",error);
+        throw error;
+    }
 }
