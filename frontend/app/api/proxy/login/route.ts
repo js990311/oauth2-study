a07ba56aeb0ecceea0d29a0e7516199b8c0ee8ac
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-10-08 23:24:38 +0900

    feat(modify): retry 로직을 스프링에도 적용

--- frontend/app/api/proxy/login/route.ts
+++ frontend/app/api/proxy/login/route.ts
@@ -1,6 +1,6 @@
 import {NextRequest, NextResponse} from "next/server";
 import {cookies} from "next/headers";
-import {LoginResponse} from "@/types/LoginResponse";
+import {withRetry} from "@/utils/refreshToken";
 
 async function fetchSpring(accessToken: string){
     const target = "http://localhost:8080/api/login";
@@ -16,24 +16,17 @@ export async function GET(request: NextRequest){
     const cookieStore = await cookies();
     const accessToken = cookieStore.get('access_token')?.value;
 
-    console.log("[/api/proxy/loigin] 시작");
-
     if(!accessToken){
         return NextResponse.json({error: 'Access token not found'}, {status: 401});
     }
 
     try {
-        const response = await fetchSpring(accessToken);
-        const data: LoginResponse = await response.json();
-
-        if(response.status !== 200){
-            if(data.code == 42){
-                console.log("[/api/proxy/login] 토큰 만료 재발급 필요");
-            }
-        }
-
-        return NextResponse.json(data, {status: response.status});
-    }catch (error) {
+        const fetchResponse = await withRetry(accessToken, async (accessToken: string): Promise<Response>  => {
+            return fetchSpring(accessToken);
+        });
+        const responseJson = await fetchResponse.json();
+        return NextResponse.json(responseJson);
+    }catch (error){
         console.log(error);
         return NextResponse.json({error: 'SERVER ERROR'}, {status: 500});
     }
