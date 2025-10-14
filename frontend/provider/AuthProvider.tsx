6fdc7a8aa4c81d2e38282d78e3a5c22525eb5b51
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-10-14 22:10:42 +0900

    feat: AuthProvider for sessionProvider

---
+++ frontend/provider/AuthProvider.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import {SessionProvider} from "next-auth/react";
+import {getServerSession} from "next-auth";
+
+export default async function AuthProvider({ children }: { children: React.ReactNode }) {
+    const session = await getServerSession();
+    return (
+        <SessionProvider session={session}>
+            {children}
+        </SessionProvider>
+    );
+}
